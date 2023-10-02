02c40367156603e25238e5763504b0f99bd1153a
Asim Masood <macbookpro2089@gmail.com>
2023-10-03 00:02:20 +0500

    Defining the DataStructure

--- SolatireGit/Stack_o_Cards.h
+++ SolatireGit/Stack_o_Cards.h
@@ -1,1 +1,38 @@
 #pragma once
+#include<vector>
+#include<stdexcept>
+
+using namespace std;
+
+class StackoCards {
+	vector<Card*> Cards;
+
+public:
+	StackoCards() :Cards() {
+
+	}
+
+	void push(Card* val) {
+		Cards.push_back(val);
+	}
+
+	Card& top() {
+		return *Cards[Cards.size() - 1];
+	}
+
+	void pop() {
+		Cards.pop_back();
+	}
+
+	int size() {
+		return Cards.size();
+	}
+
+	bool isEmpty() {
+		return Cards.empty();
+	}
+
+	
+
+
+};
